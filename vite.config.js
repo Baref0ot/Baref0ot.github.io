af77eb37d604628ef57cb3db14f70380d69c381f
WRIGHT Matthew L <matthew.wright@hexagon.com>
2025-10-14 03:37:46 -0400

    chore: clean repo and add gh-pages deploy script"

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // correct for a user site like Baref0ot.github.io
 });
