5e05837e114cdd254adad0b3ebf97c15d06406d1
WRIGHT Matthew L <matthew.wright@hexagon.com>
2025-11-19 17:11:06 -0500

    Refactor code structure for improved readability and maintainability

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -22,19 +22,19 @@ export default function Portfolio() {
       title: "Portfolio Website",
       description:
         "My developer portfolio site built with React, Tailwind, and Framer Motion.",
-      featured: true,
+      featured: false,
       tags: ["React", "Tailwind", "Framer Motion"],
       image_url: "https://knowledge.boc-group.com/wp-content/uploads/2019/08/Technology-Portfolio-Management.jpg", // or leave null
       project_url: "https://baref0ot.github.io/",
     },
     {
       id: 2,
-      title: "E-commerce API",
-      description: "Scalable backend API built with .NET Core and RabbitMQ.",
-      featured: false,
-      tags: [".NET Core", "RabbitMQ", "API"],
-      image_url: null,
-      project_url: "https://github.com/myrepo",
+      title: "PewperScewpers",
+      description: "A full stack configurable Customer Relationship Management system. Built with an Angular v16 front-end, and a .NET Core v8 Web API backend.",
+      featured: true,
+      tags: ["Angular v16", ".NET Core v8", "API", "Angular Material"],
+      image_url: "/PS_img.jpg",
+      project_url: "https://baref0ot.github.io/pewperscewpers/",
     },
     // add more projects here
   ];
